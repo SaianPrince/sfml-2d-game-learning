46faa0830da2fc0804d6dc17d02d4c34064a9ce7
SaianPrince <faruk2969@hotmail.com>
2026-02-15 19:09:51 +0300

    Implement nWindow class for event management

---
+++ part4-creatingEventClass_EventManagement/nWindow.cpp
@@ -0,0 +1,64 @@
+#include "nWindow.h"
+#include "FuncTypes.h"
+
+
+void nWindow::nCreate(unsigned int width, unsigned int length, const sf::String& text)
+{
+	newWindow.create(sf::VideoMode(width, length), text);
+}
+
+void nWindow::nDraw(const sf::Drawable& shape)
+{
+	newWindow.draw(shape);
+}
+
+void nWindow::eventManage() 
+{
+	sf::Event nEvent;
+	while (newWindow.pollEvent(nEvent)) 
+	{
+		if (nEvent.type == sf::Event::Closed) 
+		{
+			newWindow.close();
+		}
+		if (nEvent.type == sf::Event::KeyPressed) 
+		{
+			for (auto next : nKeyPressFuncList) 
+			{
+				next(nEvent.key.code);
+			}
+		}
+		if (nEvent.type == sf::Event::MouseMoved) 
+		{
+			for (auto next : nMouseMoveFuncList)
+			{
+				next(nEvent.mouseMove);
+			}
+		}
+	}
+}
+
+bool nWindow::isWindowOpen()
+{
+	return newWindow.isOpen();
+}
+
+void nWindow::nClear() 
+{
+	newWindow.clear();
+}
+
+void nWindow::show() 
+{
+	newWindow.display();
+}
+
+void nWindow::addKeyPressFunc(keyboardFunc newFunc)
+{
+	nKeyPressFuncList.push_back(newFunc);
+}
+
+void nWindow::addMouseMoveFunc(mouseMoveFunc mouseMove)
+{
+	nMouseMoveFuncList.push_back(mouseMove);
+}
