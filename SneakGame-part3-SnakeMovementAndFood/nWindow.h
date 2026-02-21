44fbc8c1c3428f1ad731a3e01d665fa122729163
SaianPrince <faruk2969@hotmail.com>
2026-02-21 19:30:02 +0300

    Create nWindow.h

---
+++ SneakGame-part3-SnakeMovementAndFood/nWindow.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "FuncTypes.h"
+
+class nWindow {
+public:
+	void nCreate(unsigned int width, unsigned int length, const sf::String& text = "SFML");
+	void nDraw(const sf::Drawable& shape);
+	void eventManage();
+	bool isWindowOpen();
+	void nClear();
+	void show();
+	void addKeyPressFunc(keyboardFunc newFunc);
+	void addMouseMoveFunc(mouseMoveFunc mouseMove);
+	void addMouseClickFunc(mouseClick click);
+private:
+
+	sf::RenderWindow newWindow;
+	keyboardFuncList nKeyPressFuncList;
+	mouseMoveFuncList nMouseMoveFuncList;
+	mouseClickList nMouseClickList;
+};
