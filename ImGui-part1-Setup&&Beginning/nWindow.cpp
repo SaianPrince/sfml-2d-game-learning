48049b20a74f222e7c33d1b9a797cb3e5b624b34
SaianPrince <faruk2969@hotmail.com>
2026-02-22 16:31:34 +0300

    nWindow.cpp

---
+++ ImGui-part1-Setup&&Beginning/nWindow.cpp
@@ -0,0 +1,97 @@
+#include "nWindow.h"
+#include "FuncTypes.h"
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+
+void nWindow::nCreate(unsigned int width, unsigned int length, const sf::String& text)
+{
+	newWindow.create(sf::VideoMode(width, length), text);
+	ImGui::SFML::Init(newWindow);
+}
+
+void nWindow::nDraw(const sf::Drawable& shape)
+{
+	newWindow.draw(shape);
+}
+
+void nWindow::eventManage() 
+{
+	sf::Event nEvent;
+	while (newWindow.pollEvent(nEvent)) 
+	{
+		ImGui::SFML::ProcessEvent(nEvent);
+
+		if (nEvent.type == sf::Event::Closed) 
+		{
+			newWindow.close();
+		}
+		if (nEvent.type == sf::Event::KeyPressed) 
+		{
+			for (auto next : nKeyPressFuncList) 
+			{
+				next(nEvent.key.code);
+			}
+		}
+		if (nEvent.type == sf::Event::MouseMoved) 
+		{
+			for (auto next : nMouseMoveFuncList)
+			{
+				next(nEvent.mouseMove);
+			}
+		}
+		if (nEvent.type == sf::Event::MouseButtonPressed) 
+		{
+			for (auto next : nMouseClickList)
+			{
+				next(nEvent.mouseButton);
+			}
+		}
+	}
+}
+
+bool nWindow::isWindowOpen()
+{
+	return newWindow.isOpen();
+}
+
+void nWindow::nClear() 
+{
+	newWindow.clear(sf::Color::White);
+}
+
+void nWindow::show() 
+{
+	
+	ImGui::SFML::Render(newWindow);
+	newWindow.display();
+}
+
+void nWindow::update(const sf::Time& time)
+{
+	ImGui::SFML::Update(newWindow, time);
+}
+
+void nWindow::addKeyPressFunc(keyboardFunc newFunc)
+{
+	nKeyPressFuncList.push_back(newFunc);
+}
+
+void nWindow::addMouseMoveFunc(mouseMoveFunc mouseMove)
+{
+	nMouseMoveFuncList.push_back(mouseMove);
+}
+
+void nWindow::addMouseClickFunc(mouseClick click)
+{
+	nMouseClickList.push_back(click);
+}
+
+void nWindow::shutDown()
+{
+	if(isWindowOpen())
+	newWindow.close();
+	
+	ImGui::SFML::Shutdown();
+
+}
